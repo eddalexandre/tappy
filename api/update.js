37ef8a16ca1d62c0d796c37efeb9bc26d6735f4a
eddalexandre <eddalexandre@gmail.com>
2026-04-03 22:51:33 -0300

    Create update.js

---
+++ api/update.js
@@ -0,0 +1,13 @@
+import { createClient } from '@vercel/kv';
+
+export default async function handler(req, res) {
+    const { id, url } = req.query;
+
+    const kv = createClient({
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN,
+    });
+
+    await kv.set(id, url);
+    return res.status(200).json({ status: "success" });
+}
