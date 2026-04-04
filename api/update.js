1eba0b4bd02540436825d9d37f7bae201221008b
eddalexandre <eddalexandre@gmail.com>
2026-04-03 22:54:56 -0300

    Update update.js

--- api/update.js
+++ api/update.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
     const { id, url } = req.query;
 
     const kv = createClient({
-        url: process.env.KV_REST_API_URL,
-        token: process.env.KV_REST_API_TOKEN,
+        url: process.env.KV_TAPPY_REST_API_URL,
+        token: process.env.KV_TAPPY_REST_API_TOKEN,
     });
 
     await kv.set(id, url);
